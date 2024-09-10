d0234868dbe755f1ca929f9018464a8eceb935b1
JamieCFreeman <jcf236@cornell.edu>
2024-09-10 17:52:02 -0500

    separate files into unkown vs known for easier regex of pop names

--- run_gt_mat.py
+++ run_gt_mat.py
@@ -51,36 +51,65 @@ all_inv = ['1A', '1Be', '2LT', '2RNS', '3LP', '3RK', '3RMO', '3RP']
 ###############################################################
 
 ref = "/home/jamie/FAS1K_utils/ref_fas1k/Reference_Chr" + arm + ".fas1k"
-d = ["/home/jamie/DGN_compatible/stock_validation/round2/fas1k", "/home/jamie/DGN_compatible/ZI_N/round2/fas1k",
-"/home/jamie/DGN_compatible/FR_N/round2/fas1k",
-"/home/jamie/Nexus_diploid_fas1k", "/home/jamie/DGN_compatible/FR_N/round2/fas1k", 
-"/raid10/backups/genepool/DPGP2plus/wrap1kb/ZI_inbred_diploid", "/raid10/backups/genepool/DPGP2plus/wrap1kb/FR_diploid",
-"/home/jamie/dpgp3_sequences", "/home/jamie/dpgp2_sequences"]
+# Files to gt
+uk = ["/home/jamie/DGN_compatible/stock_validation/round2/fas1k", "/home/jamie/DGN_compatible/FR_N/round2/fas1k", 
+        "/home/jamie/DGN_compatible/ZI_N/round2/fas1k" ]
+
+# Known set
+k = [ "/home/jamie/Nexus_diploid_fas1k",
+"/raid10/backups/genepool/DPGP2plus/wrap1kb/ZI_inbred_diploid", 
+"/raid10/backups/genepool/DPGP2plus/wrap1kb/FR_diploid",
+"/home/jamie/dpgp3_sequences", "/home/jamie/dpgp2_sequences", 
+"/raid10/jamie/diploid_fas1k_nomask/CLARK"]
 
 to_exclude = ["SD136N", "FR11N"]
 
 # For a list of directories get list of files
-all_files = []
-for x in d:
-	dl = os.listdir(x)
-	all_files += [ os.path.join(x, y) for y in dl ]
-
+def listfullpath(d):
+	out = []
+	for x in d:
+		dl = os.listdir(x)
+		out += [ os.path.join(x, y) for y in dl ]
+	return out
 
 def get_fas1k_now(s, c):
 	b =  (c in s ) & ( s.endswith(".fas1k") ) & all([ x not in s for x in to_exclude ])
 	#b =  (c in s ) & ( s.endswith(".fas1k") ) & ( "diploid" in s) & (to_exclude not in s)
 	return b
 
-l = filt_list(all_files, get_fas1k_now, arm)
-#l = l[0:100]
-# Get line name from file path
-names = [ f1k.get_name(x) for x in l]
+def get_leading_alpha(s):
+	'''
+	From a string, return characters until hit an non-alphabet character
+	'''
+	# Unpack string and test whether each char is alpha
+	l = [*s]
+	b = [ x.isalpha() for x in l ]
+	# Get first non-alpha character
+	end = [i for i, x in enumerate(b) if not x]
+	# And return up to that
+	return ''.join(l[0:end[0]])
 
+
+uk_files = listfullpath(uk)
+
+k_files  = listfullpath(k)
+m = filt_list(k_files, get_fas1k_now, arm)
+m.sort()
+# Get line name from file path
+names = [ f1k.get_name(x) for x in m]
+
+# For known sequences, first alpabetic characters are pop code
+k_sa_codes = [ x + '\t' + 'F' + '\t' +  get_leading_alpha(x) for x in names ]
+
+l = filt_list(uk_files, get_fas1k_now, arm)
+l.sort()
+names = [ f1k.get_name(x) for x in l]
 
 # List of all included populations
 pop_list = ['FR', 'ZI', 'EG', 'SD', 'SP', 'EA', 'EF', 'KM', 'NG', 'RG', 
         'CK', 'CO', 'ED', 'EZ', 'GA', 'GU', 'KN', 'KO', 'KR', 'KT', 'MW', 'RC', 'TZ', 
         'UG', 'UM', 'ZK', 'ZL', 'ZO', 'ZS' ]
+CLARK_list = ['B', 'I', 'N', 'T', 'ZH', 'ZW']
 
 # New libraries have different naming patterns, provide list of pop codes 
 #   and check library names against known codes- stop execution and return error
@@ -89,7 +118,10 @@ if sum([ 'missing' in  get_pop_code(x, pop_list) for x in names ]) > 0:
     missing  = list(compress(names, [ 'missing' in  get_pop_code(x, pop_list) for x in names ]))
     sys.exit('Error: Check provided list of population codes in sample! These samples do not have a match: ' +  ' '.join(missing) )
 
-sa_codes = [ x + '\t' + 'F' + '\t' +  get_pop_code(x, pop_list) for x in names ]
+uk_sa_codes = [ x + '\t' + 'F' + '\t' +  get_pop_code(x, pop_list) for x in names ]
+
+l = l + m
+sa_codes = uk_sa_codes + k_sa_codes
 
 ###############################################################
 
